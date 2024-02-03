4c0512cd9af2b52914a167e2e5d7b349f4e2a9c4
LSeiilova <lseiilova@icloud.com>
2024-02-03 22:36:12 +0600

    feat: config split input view

--- tip-calculator/View/SplitInputView.swift
+++ tip-calculator/View/SplitInputView.swift
@@ -9,6 +9,48 @@ import UIKit
 
 class SplitInputView: UIView {
     
+    private let headerView: HeaderView = {
+        let headerView = HeaderView()
+        headerView.configure(
+            topText: "Split",
+            bottomText: "the total")
+        return headerView
+    }()
+    
+    private lazy var decrementButton: UIButton = {
+        let button = buildButton(
+            text: "-",
+            corners: [.layerMinXMaxYCorner, .layerMinXMinYCorner])
+        return button
+    }()
+    
+    private lazy var incrementButton: UIButton = {
+        let button = buildButton(
+            text: "+",
+            corners: [.layerMaxXMinYCorner, .layerMaxXMaxYCorner])
+        return button
+    }()
+    
+    private lazy var quantityLabel: UILabel = {
+        let label = LabelFactory.build(
+            text: "1",
+            font: ThemeFont.bold(of: 20))
+        label.layer.borderWidth = 1
+        label.layer.borderColor = ThemeColor.separatorColor?.cgColor
+        return label
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            decrementButton,
+            quantityLabel,
+            incrementButton
+        ])
+        stackView.axis = .horizontal
+        stackView.spacing = 0
+        return stackView
+    }()
+    
     init() {
         super.init(frame: .zero)
         layout()
@@ -19,6 +61,36 @@ class SplitInputView: UIView {
     }
     
     private func layout() {
-        backgroundColor = .yellow
+        [headerView, stackView].forEach {
+            addSubview($0)
+        }
+        
+        stackView.snp.makeConstraints {
+            $0.top.bottom.trailing.equalToSuperview()
+        }
+        
+        [incrementButton, decrementButton].forEach { button in
+            button.snp.makeConstraints { make in
+                make.width.equalTo(button.snp.height)
+            }
+        }
+        
+        headerView.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.centerY.equalTo(stackView.snp.centerY)
+            $0.trailing.equalTo(stackView.snp.leading).offset(-24)
+            $0.width.equalTo(68)
+        }
+    }
+    
+    private func buildButton(text: String, corners: CACornerMask) -> UIButton {
+        let button = UIButton()
+        button.setTitle(text, for: .normal)
+        button.titleLabel?.font = ThemeFont.bold(of: 20)
+        button.setTitleColor(ThemeColor.textColor, for: .normal)
+        button.addRoundedCorners(corners: corners, radius: 8.0)
+        button.backgroundColor = ThemeColor.primaryColor
+        button.tintColor = ThemeColor.bg
+        return button
     }
 }
